106280079ddde1d20123223b1400b2b0ab1aed5d
postolenko <denis.postolenko@gmail.com>
2017-08-16 15:49:34 +0300

    Tiny Gummie

--- js/counter.js
+++ js/counter.js
@@ -1,11 +1,11 @@
 window.onload =function() {
 
-var countDownMonth = "Aug";
-var countDownDay = "30";
-var countDownYear = "2017";
-var countDownHours = "15";
-var countDownMinutes = "37";
-var countDownSeconds = "25";
+var countDownMonth = $(".counter_values").attr("data-month");
+var countDownDay = $(".counter_values").attr("data-day");
+var countDownYear = $(".counter_values").attr("data-year");
+var countDownHours = $(".counter_values").attr("data-hours");
+var countDownMinutes = $(".counter_values").attr("data-minutes");
+var countDownSeconds = $(".counter_values").attr("data-seconds");
 
 var countDownDate = new Date(countDownMonth + " " + countDownDay + ", " + countDownYear + " " + countDownHours + ":" + countDownMinutes + ":" + countDownSeconds).getTime();
 
@@ -49,39 +49,72 @@ var x = setInterval(function() {
 	var minutesStr = String(minutes).split("").join("</span><span>");
 	var secondsStr = String(seconds).split("").join("</span><span>");
 
- var countDownTempl = '<div class="counter-box">\
-						<div class="cell cell-1">\
-							<div class="time-val time-val-day"><span>'+ daysStr +'</span></div>\
-							<br />\
-							<div class="time-name">Дней</div>\
-						</div>\
-						<div class="cell cell-2">\
-							<div class="time-val time-val-hours"><span>'+ hoursStr +'</span></div>\
-							<br />\
-							<div class="time-name">Часов</div>\
-						</div>\
-						<div class="cell cell-3">\
-							<div class="time-val time-val-minutes"><span>'+ minutesStr +'</span></div>\
-							<br />\
-							<div class="time-name">Минут</div>\
-						</div>\
-						<div class="cell cell-4">\
-							<div class="time-val time-val-seconds"><span>'+ secondsStr +'</span></div>\
-							<br />\
-							<div class="time-name">Секунд</div>\
-						</div>\
-					</div>';
+  var countDownTempl = '<div class="counter-box">\
+            <div class="cell cell-1">\
+              <div class="time-val time-val-day"><span>'+ daysStr +'</span></div>\
+              <br />\
+              <div class="time-name">Дней</div>\
+            </div>\
+            <div class="cell cell-2">\
+              <div class="time-val time-val-hours"><span>'+ hoursStr +'</span></div>\
+              <br />\
+              <div class="time-name">Часов</div>\
+            </div>\
+            <div class="cell cell-3">\
+              <div class="time-val time-val-minutes"><span>'+ minutesStr +'</span></div>\
+              <br />\
+              <div class="time-name">Минут</div>\
+            </div>\
+            <div class="cell cell-4">\
+              <div class="time-val time-val-seconds"><span>'+ secondsStr +'</span></div>\
+              <br />\
+              <div class="time-name">Секунд</div>\
+            </div>\
+          </div>';
 
-  $(".counter").each(function() {
+  if (distance < 0 || $(".counter_values").attr("data-ziro") == "ziro" ) {
 
-  	$(this).html(countDownTempl);
-
-  });
-
-  // If the count down is finished, write some text 
-  if (distance < 0) {
     clearInterval(x);
-    // document.getElementById("demo").innerHTML = "EXPIRED";
+
+    var countDownTempl = '<div class="counter-box">\
+          <div class="cell cell-1">\
+            <div class="time-val time-val-day"><span>'+ "0</span><span>0" +'</span></div>\
+            <br />\
+            <div class="time-name">Дней</div>\
+          </div>\
+          <div class="cell cell-2">\
+            <div class="time-val time-val-hours"><span>'+ "0</span><span>0" +'</span></div>\
+            <br />\
+            <div class="time-name">Часов</div>\
+          </div>\
+          <div class="cell cell-3">\
+            <div class="time-val time-val-minutes"><span>'+ "0</span><span>0" +'</span></div>\
+            <br />\
+            <div class="time-name">Минут</div>\
+          </div>\
+          <div class="cell cell-4">\
+            <div class="time-val time-val-seconds"><span>'+ "0</span><span>0" +'</span></div>\
+            <br />\
+            <div class="time-name">Секунд</div>\
+          </div>\
+        </div>';
+
+    $(".counter").each(function() {
+
+      console.log(countDownTempl);
+
+      $(this).html(countDownTempl);
+
+    });
+
+  } else {
+
+    $(".counter").each(function() {
+
+    	$(this).html(countDownTempl);
+
+    });
+
   }
   
 }, 1000);
